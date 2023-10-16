d0836c12bbb8a3ff02b968cb9e265cd83ca6c31a
habulCoderJohnny <fokruljohnny@gmail.com>
2023-10-16 21:43:57 +0600

    Profile module done

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -5,6 +5,7 @@ import { CategoryRoutes } from '../modules/category/routes';
 import { CleaningServiceRoutes } from '../modules/cleaningServices/routes';
 import { BookingRoutes } from '../modules/booking/routes';
 import { ReviewRoutes } from '../modules/review/routes';
+import { ProfileRoutes } from '../modules/profile/routes';
 
 const router = express.Router();
 
@@ -32,6 +33,10 @@ const moduleRoutes = [
    {
     path: '/reviews',
     routes: ReviewRoutes,
+  },
+   {
+    path: '/profile',
+    routes: ProfileRoutes,
   },
 ];
 
